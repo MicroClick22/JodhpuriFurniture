d18dbb3aba6735b21a7608c9af25bdd6ce9c06ec
MicroClick22 <121496871+MicroClick22@users.noreply.github.com>
2022-12-30 11:26:42 +0530

    ok

---
+++ src/global/P-data.jsx
@@ -0,0 +1,51 @@
+const pData = [
+    {
+        imglink: "../images/sec4-1.png",
+        pName: "Gaz 6 Seater Dining Set",
+        dPrice: "58,822",
+        sPrice: "69,997"
+    },
+    {
+        imglink: "../images/sec4-2.png",
+        pName: "Grace Sheesham wood 3 1 1 Sofa Set",
+        dPrice: "56,000",
+        sPrice: "70,150"
+    },
+    {
+        imglink:"../images/sec4-3.png",
+        pName: "Scott King Size Bed with Drawer Storage",
+        dPrice: "67,225",
+        sPrice: "92,280"
+    },
+    {
+        imglink:"../images/sec4-4.png",
+        pName: "Arial 6 Seater Dining Set with Cushion Chairs",
+        dPrice: "63,562",
+        sPrice: "81,347"
+    },
+    {
+        imglink:"../images/sec4-2.png",
+        pName: "Marko King Size Bed with Storage",
+        dPrice: "63,562",
+        sPrice: "81,347"
+    },
+    {
+        imglink:"../images/sec4-1.png",
+        pName: "Drake King Size Bed without Storage",
+        dPrice: "63,562",
+        sPrice: "81,347"
+    },
+    {
+        imglink:"../images/sec4-3.png",
+        pName: "Kyra King Size Bed With Drawer Storage",
+        dPrice: "₹56,470",
+        sPrice: "81,347"
+    },
+    {
+        imglink:"../images/sec4-4.png",
+        pName: "Laurel King Size Bed without Storage",
+        dPrice: "₹30,470",
+        sPrice: "51,921"
+    },
+];
+export default pData;
